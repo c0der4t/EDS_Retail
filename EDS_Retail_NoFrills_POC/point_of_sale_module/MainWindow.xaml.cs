e039db4655a3a4b7ec1dd1a82b9cf4d0163d0147
c0der4t <monte@ekronds.co.za>
2022-08-17 17:14:28 -0600

    feat: Add live sale total. Add temp file creation for each sale to use in recovery

--- EDS_Retail_NoFrills_POC/point_of_sale_module/MainWindow.xaml.cs
+++ EDS_Retail_NoFrills_POC/point_of_sale_module/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Windows;
+using System.IO;
 using System.Windows.Input;
 
 namespace point_of_sale_module
@@ -13,6 +14,7 @@ namespace point_of_sale_module
     public partial class MainWindow : Window
     {
         private List<SaleLineItem> _activeSale;
+        private double _activeSaleTotal;
 
         public MainWindow()
         {
@@ -26,9 +28,11 @@ namespace point_of_sale_module
 
         private void NewSale()
         {
+            _activeSaleTotal = 0;
             _activeSale.Clear();
             dbgActiveSaleInfo.AutoGenerateColumns = true;
             dbgActiveSaleInfo.Columns.Clear();
+            UpdateActiveSaleTotal("0");
             NewLineItem();
         }
 
@@ -41,11 +45,38 @@ namespace point_of_sale_module
             edtSKU.Focus();
         }
 
+        private void UpdateActiveSaleTotal(string _lineItemPrice)
+        {
+            // #ToFix
+            _lineItemPrice = _lineItemPrice.Replace('.', ',');
+
+            double _convertedLineItemPrice = 0;
+            try
+            {
+                _convertedLineItemPrice = Convert.ToDouble(_lineItemPrice);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($"An exception occured while updating the sale total:\n{e.ToString()}");
+                throw;
+            }
+
+            _activeSaleTotal += _convertedLineItemPrice;
+
+            lblCurrentSaleTotal.Text = _activeSaleTotal.ToString();
+        }
 
         private void edtSKU_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Enter)
             {
+
+                //Save to a temp file
+                using (StreamWriter _tempSalesFile = new StreamWriter(@"C:\temp\sale000.txt", append: true))
+                {
+                    _tempSalesFile.WriteLine($"{edtSKU.Text}[9-9]{edtQtyNumber.Text}[9-9]{edtPrice.Text}[9-9]{edtProductDescr.Text}");
+                }
+
                 var currLineItem = new SaleLineItem();
                 currLineItem.addToSale(edtSKU.Text, edtQtyNumber.Text, edtPrice.Text, edtProductDescr.Text);
 
@@ -54,12 +85,46 @@ namespace point_of_sale_module
                 dbgActiveSaleInfo.ItemsSource = null;
                 dbgActiveSaleInfo.ItemsSource = _activeSale;
 
+                UpdateActiveSaleTotal(edtPrice.Text);
+
                 NewLineItem();
             }
 
 
 
         }
+
+        private void RecoverSale()
+        {
+            if (File.Exists(@"C:\temp\sale000.txt") == true)
+            {
+               var recoverDlgResult =  MessageBox.Show("A failed sale has been found, do you want to recover it?",
+                    "Recover sale", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                
+                if (recoverDlgResult == MessageBoxResult.Yes)
+                {
+                    //Load file
+                    string currLine = "";
+
+                    using (StreamReader _recoveredSaleFile = new StreamReader(@"C:\temp\sale000.txt"))
+                    {
+                       
+                        while ((currLine = _recoveredSaleFile.ReadLine()) != null)
+                        {
+
+                        }
+                    }
+
+                    // #Twitch
+                    //Parse file
+                    //Load items into sale
+                    //Update live sale total
+                }
+            }
+        }
+
+
+        #region UI Events
 
         private void btnQtyQuickButton1_Click(object sender, RoutedEventArgs e)
         {
@@ -85,5 +150,7 @@ namespace point_of_sale_module
         {
             NewSale();
         }
+        #endregion
+
     }
 }
