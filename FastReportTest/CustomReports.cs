580aedca06e4805708be8cb3dc68a7660b7ca232
c0der4t <monte@ekronds.co.za>
2022-08-23 18:26:39 -0600

    feat: Added fastreport testing code. Played arround with dynamic and prebuild versions

---
+++ FastReportTest/CustomReports.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using FastReport;
+using FastReport.Data;
+using FastReport.Export.Html;
+using FastReport.Export.Image;
+using FastReport.Format;
+using FastReport.Table;
+using FastReport.Utils;
+
+namespace FastReportTest
+{
+    internal class CustomReports
+    {
+        public Report TestReport()
+        {
+            Report report = new Report();
+            ReportPage page = new ReportPage();
+
+            report.Pages.Add(page);
+            page.CreateUniqueName();
+
+            page.ReportTitle = new ReportTitleBand();
+            page.ReportTitle.Height = Units.Centimeters * 4;
+            page.ReportTitle.CreateUniqueName();
+
+            TextObject titleText = new TextObject();
+            titleText.Parent = page.ReportTitle;
+            titleText.CreateUniqueName();
+            titleText.Bounds = new RectangleF(Units.Centimeters * 5, 0, Units.Centimeters * 10, Units.Centimeters * 1);
+            titleText.Font = new Font("Arial", 14, FontStyle.Bold);
+            titleText.Text = "EDS Retail Test Report";
+            titleText.HorzAlign = HorzAlign.Center;
+
+            return report;
+
+
+
+        }
+
+
+        public Report LoadReportFromFile(string pathToReportFile)
+        {
+            Report report = new Report();
+            string reportContent = File.ReadAllText(pathToReportFile);
+
+            report.Load(pathToReportFile);
+
+            return report;
+        }
+
+        public string GenerateReport(Report reportToGenerate, bool saveToFile = true, bool openAfterSave = false, string saveDirLocation = @"c:\temp")
+        {
+            reportToGenerate.Prepare();
+            reportToGenerate.SavePrepared($@"{saveDirLocation}\export.fpx");
+            HTMLExport htmlReport = new HTMLExport();
+            reportToGenerate.Export(htmlReport, $@"{saveDirLocation}\export.html");
+
+            if (saveToFile)
+            {
+                if (!Directory.Exists(saveDirLocation))
+                {
+                    throw new DirectoryNotFoundException();
+                }
+            }
+            else
+            {
+               
+
+                if (openAfterSave)
+                {
+                    Process.Start($@"{saveDirLocation}\export.html");
+                }
+            }
+            return $@"Created report export: {saveDirLocation}\export.html";
+        }
+
+    }
+}
---
+++ FastReportTest/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FastReportTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        CustomReports customReports = new CustomReports();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Run Report");
+           string result =  customReports.GenerateReport(customReports.TestReport(),true,true,@"C:\temp");
+            Debug.Write(result);
+        }
+
+        private void btnLoadFromFile_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Run Report from File");
+            string result = customReports.GenerateReport(customReports.LoadReportFromFile(@"C:\temp\receipt1.frx"), true, true, @"C:\temp");
+            Debug.Write(result);
+        }
+    }
+}
