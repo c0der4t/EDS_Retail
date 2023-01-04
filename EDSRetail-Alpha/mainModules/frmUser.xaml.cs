df92c0fe6f54a179bb55475538ec0b1be1d43fe3
c0der4t <monte@ekronds.co.za>
2023-01-03 19:17:25 -0700

    feat: Added User management form. Can now add edit and delete system users. bug: Fixed random bugs

---
+++ EDSRetail-Alpha/mainModules/frmUser.xaml.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.EntityFrameworkCore;
+using securityAPI;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Security;
+using System.Security.RightsManagement;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace mainModules
+{
+    /// <summary>
+    /// Interaction logic for frmUser.xaml
+    /// </summary>
+    public partial class frmUser : Window
+    {
+        private int userToEdit_ID = -1;
+        private string CurrPassHash = string.Empty;
+
+        public frmUser()
+        {
+            InitializeComponent();
+        }
+
+        public frmUser(databaseAPI.Models.User userToEdit)
+        {
+            InitializeComponent();
+
+            edtFirstName.Text = userToEdit.FirstName;
+            edtLastName.Text = userToEdit.LastName;
+            edtEmail.Text = userToEdit.Email;
+            edtUsername.Text = userToEdit.Username;
+            edtUserPassword.Password = userToEdit.Password;
+            chckAdmin.IsChecked = userToEdit.IsAdmin;
+            chckCanLogin.IsChecked = userToEdit.CanLogin;
+            chckChangePassw.IsChecked = userToEdit.MustChangePassword;
+
+            userToEdit_ID = userToEdit.ID;
+            CurrPassHash = userToEdit.Password;
+            
+        }
+
+        private void btnPostUserSetup_Click(object sender, RoutedEventArgs e)
+        {
+            if (edtUserPassword.Password != string.Empty)
+            {
+
+                databaseAPI.Models.User systemUser = new databaseAPI.Models.User();
+
+                systemUser.FirstName = edtFirstName.Text;
+                systemUser.LastName = edtLastName.Text;
+                systemUser.Email = edtEmail.Text;
+
+                systemUser.Username = edtUsername.Text;
+                systemUser.Password = edtUserPassword.Password == CurrPassHash ? CurrPassHash : Encryption.GenerateHashFromString(edtUserPassword.Password);
+                systemUser.IsAdmin = (bool)chckAdmin.IsChecked;
+                systemUser.CanLogin = (bool)chckCanLogin.IsChecked;
+                systemUser.MustChangePassword = (bool)chckCanLogin.IsChecked;
+
+                if (userToEdit_ID > -1)
+                {
+                    //We are in Edit mode. Delete user entry first, then add as new entry
+                    systemUser.ID = userToEdit_ID;
+
+
+
+                    using (UserContext _contextUser = new UserContext())
+                    {
+                        _contextUser.Users.Update(systemUser);
+
+                        _contextUser.SaveChanges();
+                    }
+                }
+                else
+                {
+                    //Not in edit mode, just add to DB
+                    using (UserContext _contextUser = new UserContext())
+                    {
+                        _contextUser.Users.Add(systemUser);
+
+                        _contextUser.SaveChanges();
+                    }
+                }
+
+                this.Close();
+
+            }
+            else
+            {
+                MessageBox.Show("Password cannot be empty", "Invalid Password", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            
+
+        }
+    }
+}
--- EDSRetail-Alpha/mainModules/frmsettings_main.xaml.cs
+++ EDSRetail-Alpha/mainModules/frmsettings_main.xaml.cs
@@ -52,15 +52,53 @@ namespace mainModules
                 _contextUser.Users.Local.ToObservableCollection();
         }
 
-        private void dbgUsers_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
-        {
-            //ToDo : Build an edit user box
 
-            //Check if it is the password block
-            if (e.Column.Header.ToString() == "Password")
+        private void btnAddUser_Click(object sender, RoutedEventArgs e)
+        {
+            frmUser user = new frmUser();
+            user.ShowDialog();
+            RefreshUsers();
+        }
+
+        private void RefreshUsers()
+        {
+            _contextUser.Dispose();
+            _contextUser = new UserContext();
+            InitDB();
+        }
+
+        private void btnDeleteUser_Click(object sender, RoutedEventArgs e)
+        {
+            if ((dbgUsers.SelectedIndex >= 0) && (dbgUsers.Items.Count > 1))
             {
-               string HashedString = Encryption.GenerateHashFromString(((TextBox)e.EditingElement).Text);
-               Debug.WriteLine(HashedString);
+
+                databaseAPI.Models.User userEntry = dbgUsers.SelectedItem as databaseAPI.Models.User;
+
+                MessageBoxResult mrConfirmDelete = MessageBox.Show($"Are you sure you'd like to stage this item for deletion?\nUsername = {userEntry.Username}",
+                    "WARNING: Confirm User Deletion", MessageBoxButton.YesNo);
+
+                if (mrConfirmDelete == MessageBoxResult.Yes)
+                {
+                    _contextUser.Users.Remove(userEntry);
+                    _contextUser.SaveChanges();
+
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Select a user before clicking [DELETE] (Note, you must have at least 1 user at all times)", "Unable to delete", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+            }
+        }
+
+        private void dbgUsers_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (dbgUsers.SelectedIndex >= 0)
+            {
+                databaseAPI.Models.User selectedUser = dbgUsers.SelectedItem as databaseAPI.Models.User;
+                frmUser user = new frmUser(selectedUser);
+                user.ShowDialog();
+                RefreshUsers();
             }
         }
     }
